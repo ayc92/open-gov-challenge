950efae666f8cd0b746d80134794c78e1f4c2dc1
Andrew Chen <andrew@ayc92-VB.(none)>
2013-11-17 02:14:07 -0800

    Added total field and changed some formatting.

--- app.py
+++ app.py
@@ -13,10 +13,8 @@ def file(file):
     csv_file = request.files[file]
     csv_file.save('/tmp/temp.csv')
     # then open with csv module
-    output_hash = { 'success': 'false',
-                    'excel_rows_parsed': [],
-                    'aggregations': {},
-                    'name_lookup': {'funds': {}, 'departments': {}} }
+    output_hash = { 'excel_rows_parsed': [],
+                    'aggregations': {} }
     fields = {}
     with open('/tmp/temp.csv', 'rU') as csvfile:
         reader = csv.reader(csvfile)
@@ -35,53 +33,57 @@ def file(file):
                     year = int(row[fields['Year']])
                     month = row[fields['Month']]
                     fid = int(row[fields['Fund ID']])
-                    if fid < 10:
-                        fid = '0' + str(fid)
-                    else:
-                        fid = str(fid)
                     fname = row[fields['Fund Name']]
                     did = int(row[fields['Department ID']])
-                    if did < 10:
-                        did = '0' + str(did)
-                    else:
-                        did = str(did)
                     dname = row[fields['Department Name']]
                     oname = row[fields['Object Name']]
                     amount = float(row[fields['Amount']])
                     if year in output_hash['aggregations']:
-                        if amount > 0:
-                            if fname in output_hash['aggregations'][year]['revenues']['funds']:
-                                output_hash['aggregations'][year]['revenues']['funds'][fname] += amount
-                            else:
-                                output_hash['aggregations'][year]['revenues']['funds'][fname] = amount
-                            if dname in output_hash['aggregations'][year]['revenues']['departments']:
-                                output_hash['aggregations'][year]['revenues']['departments'][dname] += amount
-                            else:
-                                output_hash['aggregations'][year]['revenues']['departments'][dname] = amount
+                        # any necessary initialization
+                        if fname not in output_hash['aggregations'][year]['revenues']['funds']:
+                            output_hash['aggregations'][year]['revenues']['funds'][fname] = 0.0
+                        if fname not in output_hash['aggregations'][year]['expenses']['funds']:
+                            output_hash['aggregations'][year]['expenses']['funds'][fname] = 0.0
+                        if dname not in output_hash['aggregations'][year]['revenues']['departments']:
+                            output_hash['aggregations'][year]['revenues']['departments'][dname] = 0.0
+                        if dname not in output_hash['aggregations'][year]['expenses']['departments']:
+                            output_hash['aggregations'][year]['expenses']['departments'][dname] = 0.0
+                        if amount > 0.0:
+                            output_hash['aggregations'][year]['revenues']['funds'][fname] += amount
+                            output_hash['aggregations'][year]['revenues']['departments'][dname] += amount
+                            output_hash['aggregations'][year]['revenues']['total'] += amount
                         else:
-                            if fname in output_hash['aggregations'][year]['expenses']['funds']:
-                                output_hash['aggregations'][year]['expenses']['funds'][fname] += amount
-                            else:
-                                output_hash['aggregations'][year]['expenses']['funds'][fname] = amount
-                            if dname in output_hash['aggregations'][year]['expenses']['departments']:
-                                output_hash['aggregations'][year]['expenses']['departments'][dname] += amount
-                            else:
-                                output_hash['aggregations'][year]['expenses']['departments'][dname] = amount
+                            output_hash['aggregations'][year]['expenses']['funds'][fname] += amount
+                            output_hash['aggregations'][year]['expenses']['departments'][dname] += amount
+                            output_hash['aggregations'][year]['expenses']['total'] += amount
                     else:
-                        if amount > 0:
+                        if amount > 0.0:
                             output_hash['aggregations'][year] = \
-                                {'revenues': {'funds': {fname: amount}, 'departments': {dname: amount}},
-                                 'expenses': {'funds': {}, 'departments': {}}}
+                                {'revenues': {'funds': {fname: amount}, 'departments': {dname: amount}, 'total': amount},
+                                 'expenses': {'funds': {fname: 0.0}, 'departments': {dname: 0.0}, 'total': 0.0}}
                         else:
                             output_hash['aggregations'][year] = \
-                                {'revenues': {'funds': {}, 'departments': {}},
-                                 'expenses': {'funds': {fname: amount}, 'departments': {dname: amount}}}
-                    if fid not in output_hash['name_lookup']['funds']:
-                        output_hash['name_lookup']['funds'][fname] = fname
-                    if did not in output_hash['name_lookup']['departments']:
-                        output_hash['name_lookup']['departments'][dname] = dname
+                                {'revenues': {'funds': {fname: 0.0}, 'departments': {dname: 0.0}, 'total': 0.0},
+                                 'expenses': {'funds': {fname: amount}, 'departments': {dname: amount}, 'total': amount}}
             row_idx += 1
-    output_hash['success'] = True
+    # now we need to format the floats as fixed precision (rounded to the tenths place)
+    for yr in output_hash['aggregations']:
+        for fun in output_hash['aggregations'][yr]['expenses']['funds']:
+            output_hash['aggregations'][yr]['expenses']['funds'][fun] = \
+                float('%.2f' % round(output_hash['aggregations'][yr]['expenses']['funds'][fun], 2))
+        for fun in output_hash['aggregations'][yr]['revenues']['funds']:
+            output_hash['aggregations'][yr]['revenues']['funds'][fun] = \
+                float('%.2f' % round(output_hash['aggregations'][yr]['revenues']['funds'][fun], 2))
+        for dep in output_hash['aggregations'][yr]['expenses']['departments']:
+            output_hash['aggregations'][yr]['expenses']['departments'][dep] = \
+                float('%.2f' % round(output_hash['aggregations'][yr]['expenses']['departments'][dep], 2))
+        for dep in output_hash['aggregations'][yr]['revenues']['departments']:
+            output_hash['aggregations'][yr]['revenues']['departments'][dep] = \
+                float('%.2f' % round(output_hash['aggregations'][yr]['revenues']['departments'][dep], 2))
+        output_hash['aggregations'][yr]['expenses']['total'] = \
+            float('%.2f' % round(output_hash['aggregations'][yr]['expenses']['total'], 2))
+        output_hash['aggregations'][yr]['revenues']['total'] = \
+            float('%.2f' % round(output_hash['aggregations'][yr]['revenues']['total'], 2))
     json_resp = jsonify(**output_hash)
     return json_resp
 
